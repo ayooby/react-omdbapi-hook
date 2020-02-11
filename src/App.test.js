12e7fb20835a929e353c8f7091e472d792f8ebee
Sajad Ayooby <sajad666666@gmail.com>
2020-02-11 20:26:36 +0100

    make simple test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('index is rendered correctly', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Welcome to OMDB applicaton/i);
   expect(linkElement).toBeInTheDocument();
 });
